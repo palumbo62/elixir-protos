f4fe48fa77b474be5a4108d6b98ba3e5be03c908
rpalumboHFHS <robert.palumbo@holyfamilyhs.com>
2021-11-22 13:06:41 -0700

    Add changeset validations

--- lib/be_exercise/countries/country.ex
+++ lib/be_exercise/countries/country.ex
@@ -1,7 +1,8 @@
 defmodule Exercise.Countries.Country do
   use Ecto.Schema
   import Ecto.Changeset
-
+  @primary_key {:id, :id, autogenerate: true}
+  
   schema "countries" do
     field :code, :string
     field :name, :string
@@ -14,8 +15,9 @@ defmodule Exercise.Countries.Country do
   def changeset(country, attrs) do
     country
     |> cast(attrs, [:name, :code, :currency_id])
-    |> validate_required([:name, :code])
-    |> validate_number(:currency_id, greater_than: 0)      
+    |> validate_required([:name, :code, :currency_id])
+    |> validate_number(:currency_id, greater_than: 0)   
+    |> foreign_key_constraint(:currency_id)   
     |> unique_constraint(:name)
     |> unique_constraint(:code)
   end
