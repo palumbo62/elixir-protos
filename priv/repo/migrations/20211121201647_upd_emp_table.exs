af2b33448d616761e6e2ad37056a686772b86f9c
rpalumboHFHS <robert.palumbo@holyfamilyhs.com>
2021-11-22 13:26:33 -0700

    Additional database migrations for various development states

---
+++ priv/repo/migrations/20211121201647_upd_emp_table.exs
@@ -0,0 +1,19 @@
+defmodule Exercise.Repo.Migrations.CreateEmployees do
+  use Ecto.Migration
+
+  def change do
+    create table(:employees) do
+      add :first_name, :string
+      add :last_name, :string
+      add :job_title, :string
+      add :salary, :numeric
+      add :emp_id, :integer
+      add :country_id, references(:countries, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create unique_index(:employees, [:emp_id])
+    create index(:employees, [:country_id])
+  end
+end
