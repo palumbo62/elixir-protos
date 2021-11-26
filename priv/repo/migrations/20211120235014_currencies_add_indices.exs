9eda31173a22b8e48a275749a1ce4f9760d842b2
rpalumboHFHS <robert.palumbo@holyfamilyhs.com>
2021-11-26 12:20:55 -0700

    Removed - no longer needed

--- priv/repo/migrations/20211120235014_currencies_add_indices.exs
+++
@@ -1,8 +0,0 @@
-defmodule Exercise.Repo.Migrations.CurrenciesAddIndices do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:currencies, [:name])
-    create unique_index(:currencies, [:code])
-  end
-end
